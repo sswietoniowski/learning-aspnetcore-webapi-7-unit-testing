07c0e0a6322c8fcb6605c0943e904deffc3ae45d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-03-19 14:15:33 +0100

    Added HR repositories

---
+++ hr/api/Services/HrRepository.cs
@@ -0,0 +1,50 @@
+using Hr.Api.DataAccess;
+using Hr.Api.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hr.Api.Services;
+
+public class HrRepository : IHrRepository
+{
+    private readonly HrDbContext _context;
+
+    public HrRepository(HrDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<IEnumerable<InternalEmployee>> GetInternalEmployeesAsync() => 
+        await _context.InternalEmployees
+            .Include(e => e.AttendedCourses)
+            .ToListAsync();
+
+    public InternalEmployee? GetInternalEmployeeById(Guid employeeId) =>
+        _context.InternalEmployees
+            .Include(e => e.AttendedCourses)
+            .FirstOrDefault(e => e.Id == employeeId);
+
+    public async Task<InternalEmployee?> GetInternalEmployeeByIdAsync(Guid employeeId) =>
+        await _context.InternalEmployees
+            .Include(e => e.AttendedCourses)
+            .FirstOrDefaultAsync(e => e.Id == employeeId);
+
+    public void CreateInternalEmployee(InternalEmployee internalEmployee)
+    {
+        _context.InternalEmployees.Add(internalEmployee);
+    }
+
+    public Course? GetCourseById(Guid courseId) =>
+        _context.Courses.FirstOrDefault(c => c.Id == courseId);
+
+    public async Task<Course?> GetCourseByIdAsync(Guid courseId) =>
+        await _context.Courses.FirstOrDefaultAsync(c => c.Id == courseId);
+
+    public List<Course> GetCoursesByIds(params Guid[] courseIds) =>
+        _context.Courses.Where(c => courseIds.Contains(c.Id)).ToList();
+
+    public async Task<List<Course>> GetCoursesByIdsAsync(params Guid[] courseIds) =>
+        await _context.Courses.Where(c => courseIds.Contains(c.Id)).ToListAsync();
+
+    public async Task SaveChangesAsync() =>
+        await _context.SaveChangesAsync();
+}
---
+++ hr/api/Services/IHrRepository.cs
@@ -0,0 +1,16 @@
+using Hr.Api.DataAccess.Entities;
+
+namespace Hr.Api.Services;
+
+public interface IHrRepository
+{
+    Task<IEnumerable<InternalEmployee>> GetInternalEmployeesAsync();
+    InternalEmployee? GetInternalEmployeeById(Guid employeeId);
+    Task<InternalEmployee?> GetInternalEmployeeByIdAsync(Guid employeeId);
+    void CreateInternalEmployee(InternalEmployee internalEmployee);
+    List<Course> GetCoursesByIds(params Guid[] courseIds);
+    Task<List<Course>> GetCoursesByIdsAsync(params Guid[] courseIds);
+    Course? GetCourseById(Guid courseId);
+    Task<Course?> GetCourseByIdAsync(Guid courseId);
+    Task SaveChangesAsync();
+}
