e91bf917594c0f537b79a9d2610fabd02440169e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-03-23 16:00:58 +0100

    Created basic tests

---
+++ hr/api_tests/CourseTests.cs
@@ -0,0 +1,19 @@
+﻿using Hr.Api.DataAccess.Entities;
+
+namespace Hr.Api.Tests;
+
+public class CourseTests
+{
+    [Fact]
+    public void CourseConstructor_ConstructCourse_IsNewMustBeTrue()
+    {
+        // Arrange
+        // nothing to see here
+
+        // Act
+        var course = new Course("Disaster Management 101");
+
+        // Assert
+        Assert.True(course.IsNew);
+    }
+}
